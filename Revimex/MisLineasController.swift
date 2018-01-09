aac7378d1ae7d7430e0fb577a0fc634af7054e7b
seiferk01 <seifer_k01@hotmail.com>
2018-01-08 19:22:01 -0600

    08-12-2018

--- Revimex/MisLineasController.swift
+++ Revimex/MisLineasController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class MisLineasController: UIViewController {
     
+    @IBOutlet weak var vistasContainer: UIView!
+    var misPropiedadesBtn = UIButton()
+    var misInversionesBtn = UIButton()
+    var misBrokeragesBtn = UIButton()
+    
     var tapGesture: UITapGestureRecognizer!;
     var menuContainer: UIView!;
+    
+    var misPropiedades:UIViewController?
+    var misInversiones:UIViewController?
+    var misBrokerages:UIViewController?
+    
+    var arrayViews:[UIViewController?]!;
     
     
     override func viewDidLoad() {
@@ -22,9 +33,13 @@ class MisLineasController: UIViewController {
         
         self.navigationController?.isNavigationBarHidden = false;
         
+        crearTabs()
+        
         menuContextual()
         
         iniMenu();
+        
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -33,13 +48,14 @@ class MisLineasController: UIViewController {
         super.viewWillDisappear(animated);
     }
     
+    
+    //**************************funciones para crear el menu contextual**********************************
     func menuContextual(){
         imagenCuentaBtn.setBackgroundImage(UIImage(named:"menu-horizontal-100.png"), for: .normal);
         tapGesture = UITapGestureRecognizer(target: self,action: #selector(menuTapped(tapGestureRecognizer:)));
         imagenCuentaBtn.addGestureRecognizer(tapGesture);
     }
     
-    //Crear menu contextual
     private func iniMenu(){
         
         let screenSize = UIScreen.main.bounds;
@@ -81,12 +97,6 @@ class MisLineasController: UIViewController {
         view.addSubview(menuContainer);
     }
     
-    
-    func crearTabs(){
-        
-    }
-    
-    
     @objc func menuTapped(tapGestureRecognizer: UITapGestureRecognizer){
         menuContainer.isHidden = !menuContainer.isHidden
     }
@@ -104,6 +114,102 @@ class MisLineasController: UIViewController {
         performSegue(withIdentifier: "infoToUserInfo", sender: nil)
         menuContainer.isHidden = true
     }
+    
+    //*****************************funciones para manejo de las vistas******************************
+    func crearTabs(){
+        
+        let tapMisPropiedades = UITapGestureRecognizer(target: self, action: #selector(mostrarMisPropiedades(tapGestureRecognizer:)))
+        let tapMisInversiones = UITapGestureRecognizer(target: self, action: #selector(mostrarMisInversiones(tapGestureRecognizer:)))
+        let tapMisBrokerages = UITapGestureRecognizer(target: self, action: #selector(mostrarMisBrokerages(tapGestureRecognizer:)))
+        
+        misPropiedadesBtn = tabStyle(position: 0,tab: misPropiedadesBtn,titulo: "Mis Propiedades")
+        misInversionesBtn = tabStyle(position: 1,tab: misInversionesBtn,titulo: "Mis Inversiones")
+        misBrokeragesBtn = tabStyle(position: 2,tab: misBrokeragesBtn,titulo: "Mis Brokerages")
+        
+        misPropiedadesBtn.addGestureRecognizer(tapMisPropiedades)
+        misInversionesBtn.addGestureRecognizer(tapMisInversiones)
+        misBrokeragesBtn.addGestureRecognizer(tapMisBrokerages)
+        
+        view.addSubview(misPropiedadesBtn)
+        view.addSubview(misInversionesBtn)
+        view.addSubview(misBrokeragesBtn)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        misPropiedades = storyboard.instantiateViewController(withIdentifier: "MisPropiedades") as! MisPropiedadesController
+        misInversiones = storyboard.instantiateViewController(withIdentifier: "MisInversiones") as! InversionistaController
+        misBrokerages = storyboard.instantiateViewController(withIdentifier: "MisBrokerages") as! MisBrokerageViewController
+        
+        arrayViews = [misPropiedades,misInversiones,misBrokerages]
+        
+        actualViewController = arrayViews[0];
+        
+    }
+    
+    func tabStyle(position: Int, tab: UIButton,titulo: String)->UIButton{
+        
+        let ancho = view.bounds.width * 0.333
+        let largo = view.bounds.height * 0.05
+        let top = (navigationController?.navigationBar.bounds.height)!+20
+        
+        vistasContainer.frame = CGRect(x:0,y:top + largo,width:view.bounds.width,height:view.bounds.height - top - largo)
+        misPropiedadesBtn.setTitleColor(azul, for: .normal)
+        
+        tab.frame = CGRect(x:ancho*CGFloat(position), y: top, width: ancho, height:largo)
+        tab.setTitle(titulo, for: .normal)
+        tab.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14.0)
+        tab.setTitleColor(UIColor.white, for: .normal)
+        tab.layer.borderWidth = 0.5
+        tab.layer.borderColor = UIColor.white.cgColor
+        
+        return tab
+    }
+    
+    private var actualViewController: UIViewController?{
+        didSet{
+            removeInactiveViewController(inactiveViewController: oldValue);
+            updateActiveViewController();
+        }
+    }
+    
+    private func removeInactiveViewController(inactiveViewController: UIViewController?){
+        if let inactiveVC = inactiveViewController{
+            inactiveVC.willMove(toParentViewController: nil);
+            inactiveVC.view.removeFromSuperview();
+            inactiveVC.removeFromParentViewController();
+        }
+    }
+    
+    private func updateActiveViewController(){
+        if let activeVC = actualViewController{
+            addChildViewController(activeVC);
+            activeVC.view.frame = vistasContainer.bounds;
+            vistasContainer.addSubview(activeVC.view);
+            activeVC.didMove(toParentViewController: self);
+        }
+    }
+    
+    @objc func mostrarMisPropiedades(tapGestureRecognizer: UITapGestureRecognizer){
+        misPropiedadesBtn.setTitleColor(azul, for: .normal)
+        misInversionesBtn.setTitleColor(UIColor.white, for: .normal)
+        misBrokeragesBtn.setTitleColor(UIColor.white, for: .normal)
+        actualViewController = arrayViews[0];
+    }
+    
+    @objc func mostrarMisInversiones(tapGestureRecognizer: UITapGestureRecognizer){
+        misPropiedadesBtn.setTitleColor(UIColor.white, for: .normal)
+        misInversionesBtn.setTitleColor(azul, for: .normal)
+        misBrokeragesBtn.setTitleColor(UIColor.white, for: .normal)
+        actualViewController = arrayViews[1];
+    }
+    
+    @objc func mostrarMisBrokerages(tapGestureRecognizer: UITapGestureRecognizer){
+        misPropiedadesBtn.setTitleColor(UIColor.white, for: .normal)
+        misInversionesBtn.setTitleColor(UIColor.white, for: .normal)
+        misBrokeragesBtn.setTitleColor(azul, for: .normal)
+        actualViewController = arrayViews[2];
+    }
+    
 
 
 }
